d89054aa4a6763914d5483b7a51cb312e275a80f
Atul <135580429+Atul-k-m@users.noreply.github.com>
2023-10-27 15:52:37 +0530

    Create insertionSort.js

---
+++ C++/insertionSort.js
@@ -0,0 +1,20 @@
+function insertionSort(arr) {
+  const n = arr.length;
+  
+  for (let i = 1; i < n; i++) {
+    const key = arr[i];
+    let j = i - 1;
+    
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    
+    arr[j + 1] = key;
+  }
+}
+
+// Example usage:
+const arrayToSort = [12, 11, 13, 5, 6];
+insertionSort(arrayToSort);
+console.log("Sorted array:", arrayToSort);
